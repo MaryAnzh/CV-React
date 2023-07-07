4ee6989ced8e2ab46cbab55e9306a7e07d56d146
MaryAnzh <maryang@mail.ru>
2023-07-07 11:35:24 +0300

    feat: add burger nav panel

--- src/components/common/burger/burger.styled.ts
+++ src/components/common/burger/burger.styled.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
-export const BurgerStyle = styled.div<{ open: boolean }>`
-   display: none;
+export const BurgerStyle = styled.div`
+  display: none;
+  position: relative;
+  width: ${({ theme }) => theme.constants.iconSize};
+  height: ${({ theme }) => theme.constants.iconSize};
+   
+  @media (max-width: ${({ theme }) => theme.constants.mediaTabletPoint}) {
+    display: flex;
+  }
+
+`;
+
+export const BurgerIconStyle = styled.div<{ open: boolean }>`
+   display: flex;
    position: relative;
    width: ${({ theme }) => theme.constants.iconSize};
    height: ${({ theme }) => theme.constants.iconSize};
@@ -12,6 +24,7 @@ export const BurgerStyle = styled.div<{ open: boolean }>`
    opacity: 0.8;
    transition: all .3s;
    cursor: pointer;
+   z-index: 2;
 
    &:hover {
     opacity: 1;
@@ -35,10 +48,6 @@ export const BurgerStyle = styled.div<{ open: boolean }>`
      transform: rotate(${({ open }) => open ? '-45deg' : '0'});
      transition: top ${({ open }) => open ? '.2s' : '.2s .2s'}, transform ${({ open }) => open ? '.2s .2s' : '.2s'};
     }
-
-   @media (max-width: ${({ theme }) => theme.constants.mediaTabletPoint}) {
-    display: flex;
-  }
 `;
 
 export const BurgerLine = styled.div`
@@ -48,4 +57,36 @@ export const BurgerLine = styled.div`
   height: ${({ theme }) => theme.constants.burgerLineHight};
   border-radius: 2px;
   background: ${({ theme }) => theme.colors.contrast};
+`;
+
+export const BurgerNavPanel = styled.ul`
+  position: absolute;
+  right: -0.4rem;
+  top: -0.4rem;
+  padding: calc(1rem + ${({ theme }) => theme.constants.iconSize}) 0;
+  padding-bottom: 1rem;
+  border: 1px solid ${({ theme }) => theme.colors.contrast};
+  border-radius: 6px;
+  z-index: 1;
+`;
+
+export const BurgerNavItem = styled.li`
+  padding: 0 1rem;
+  text-transform: uppercase;
+  transition: all.3s;
+  
+  a {
+    color: ${({ theme }) => theme.colors.contrast};
+    font-size: 1.25rem;
+    font-weight: 100;
+    transition: all.3s;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.contrast};
+    
+    a {
+      color: ${({ theme }) => theme.colors.main};
+    } 
+  }
 `;
--- src/components/common/burger/burger.tsx
+++ src/components/common/burger/burger.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { navList } from "../../../pages/pages-const";
+import { Text } from "../../smart/text/text";
+
 import {
     BurgerStyle,
-    BurgerLine
+    BurgerIconStyle,
+    BurgerLine,
+    BurgerNavPanel,
+    BurgerNavItem
 } from "./burger.styled";
 
 export const Burger = () => {
+    const listItem = navList.map((el) => {
+        return (
+            <BurgerNavItem key={el.name}>
+                <Link to={el.path}>
+                    <Text tid={el.translateKey} />
+                </Link>
+            </BurgerNavItem>
+        )
+    });
+
     const [open, setOpen] = useState(false);
 
     const onClick = () => {
@@ -12,13 +30,18 @@ export const Burger = () => {
     }
 
     return (
-        <BurgerStyle
-            className='burger'
-            open={open}
-            onClick={onClick}>
-            <BurgerLine></BurgerLine>
-            <BurgerLine></BurgerLine>
-            <BurgerLine></BurgerLine>
+        <BurgerStyle className='burger'>
+            <BurgerIconStyle
+                className='burger__icon'
+                open={open}
+                onClick={onClick}>
+                <BurgerLine></BurgerLine>
+                <BurgerLine></BurgerLine>
+                <BurgerLine></BurgerLine>
+            </BurgerIconStyle>
+            <BurgerNavPanel className='burger__panel'>
+                {listItem}
+            </BurgerNavPanel>
         </BurgerStyle>
     );
 }
