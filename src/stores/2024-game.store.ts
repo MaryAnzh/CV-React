2007951ad890d052a9dbda91e10345d60a351bb3
MaryAnzh <maryang@mail.ru>
2023-09-24 13:20:47 +0300

    feat: add base game store

---
+++ src/stores/2024-game.store.ts
@@ -0,0 +1,25 @@
+import { action, makeObservable, observable } from "mobx";
+
+export class Store {
+  point = 0;
+
+  constructor() {
+    makeObservable(this, {
+      point: observable,
+      setPoint: action,
+    });
+  }
+
+  //setters
+  setPoint = (value: number) => {
+    this.point += value;
+  }
+
+  //round logic
+  // startNewGame
+  // startRound
+  // finishRound
+
+  //utils
+  //checkResult
+}
