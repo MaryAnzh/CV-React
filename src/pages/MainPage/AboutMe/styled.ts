90f04fef9c73ca2479a86d7dc3b687a99ee98466
MaryAnzh <maryang@mail.ru>
2026-01-20 14:47:57 +0300

    develop(feature): add grid

--- src/pages/MainPage/AboutMe/styled.ts
+++ src/pages/MainPage/AboutMe/styled.ts
@@ -10,22 +10,30 @@ export const AboutMePageWrap = styled(AccountPAgeWrap)`
     gap: var(--p-2);
     align-items: center;
     justify-content: center;
+
+    & > section:nth-of-type(3) {
+        background: var(--gray-700);
+    }  
     
     @media (min-width: ${MIN_LAPTOP}) {
         display: grid;
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: 30% 10% 30% 30%;
         grid-template-rows: repeat(2, 1fr);
 
         & > section:nth-of-type(1) {
-            grid-column: 1/2;
+            grid-column: 1/3;
         }
 
         & > section:nth-of-type(2) {
-            grid-column: 2/4;
+            grid-column: 3/5;
         }
 
+        & > section:nth-of-type(3) {
+            grid-column: 1/2;
+        }   
+
         & > section:nth-of-type(4) {
-            background: var(--gray-700);
+            grid-column: 2/4;
         }
     }
 `;
